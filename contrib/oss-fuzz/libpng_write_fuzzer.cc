eccdd63e89ee779833e3c71d129e6d1f2c17118a
Rodrigue de Guerre <99824035+Rodrigue2g@users.noreply.github.com>
2025-05-04 15:52:01 +0200

    Update libpng_write_fuzzer.cc

--- contrib/oss-fuzz/libpng_write_fuzzer.cc
+++ contrib/oss-fuzz/libpng_write_fuzzer.cc
@@ -3,6 +3,7 @@
 // Copyright 2015 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that may
 // be found in the LICENSE file https://cs.chromium.org/chromium/src/LICENSE
+
 #include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
@@ -13,8 +14,43 @@
 #define PNG_INTERNAL
 #include "png.h"
 
+#define PNG_CLEANUP \
+  if(png_handler.png_ptr) \
+  { \
+    if (png_handler.row_ptr) \
+      png_free(png_handler.png_ptr, png_handler.row_ptr); \
+    if (png_handler.end_info_ptr) \
+      png_destroy_write_struct(&png_handler.png_ptr, &png_handler.info_ptr); \
+    else if (png_handler.info_ptr) \
+      png_destroy_write_struct(&png_handler.png_ptr, &png_handler.info_ptr); \
+    else \
+      png_destroy_write_struct(&png_handler.png_ptr, nullptr); \
+    png_handler.png_ptr = nullptr; \
+    png_handler.row_ptr = nullptr; \
+    png_handler.info_ptr = nullptr; \
+    png_handler.end_info_ptr = nullptr; \
+  }
+
 struct WriteBuffer {
   std::vector<uint8_t> data;
+};
+
+struct PngObjectHandler {
+  png_infop info_ptr = nullptr;
+  png_structp png_ptr = nullptr;
+  png_infop end_info_ptr = nullptr;
+  png_voidp row_ptr = nullptr;
+  WriteBuffer* write_buf = nullptr;
+
+  ~PngObjectHandler() {
+    if (row_ptr)
+      png_free(png_ptr, row_ptr);
+    if (info_ptr)
+      png_destroy_write_struct(&png_ptr, &info_ptr);
+    else
+      png_destroy_write_struct(&png_ptr, nullptr);
+    delete write_buf;
+  }
 };
 
 void user_write_data(png_structp png_ptr, png_bytep data, png_size_t length) {
@@ -42,8 +78,6 @@ void default_free(png_structp png_ptr, png_voidp ptr) {
 }
 
 // Entry point for LibFuzzer.
-// Roughly follows the libpng book example:
-// http://www.libpng.org/pub/png/book/chapter15.html
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   if (size < 16) {
     return 0; // Not enough data to construct even minimal header.
@@ -62,62 +96,72 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
   if (width == 0 || height == 0 || width > 4096 || height > 4096)
     return 0;
 
-  png_structp png_ptr = nullptr;
-  png_infop info_ptr = nullptr;
-  png_voidp row_ptr = nullptr;
+  PngObjectHandler png_handler;
+  png_handler.png_ptr = nullptr;
+  png_handler.row_ptr = nullptr;
+  png_handler.info_ptr = nullptr;
+  png_handler.end_info_ptr = nullptr;
 
-  // Error handling block that uses goto for cleanup
-  do {
-    png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
-    if (!png_ptr) break;
+  png_handler.png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
+  if (!png_handler.png_ptr) {
+    return 0;
+  }
 
-    // Custom memory functions
-    png_set_mem_fn(png_ptr, nullptr, limited_malloc, default_free);
+  png_handler.info_ptr = png_create_info_struct(png_handler.png_ptr);
+  if (!png_handler.info_ptr) {
+    PNG_CLEANUP
+    return 0;
+  }
 
-    info_ptr = png_create_info_struct(png_ptr);
-    if (!info_ptr) break;
+  // Use a custom allocator that fails for large allocations to avoid OOM.
+  png_set_mem_fn(png_handler.png_ptr, nullptr, limited_malloc, default_free);
 
-    if (setjmp(png_jmpbuf(png_ptr))) break;
+  // Setting up writing to buffer
+  png_handler.write_buf = new WriteBuffer();
+  png_set_write_fn(png_handler.png_ptr, png_handler.write_buf, user_write_data, user_flush_data);
 
-    WriteBuffer out_buf;
-    png_set_write_fn(png_ptr, &out_buf, user_write_data, user_flush_data);
+  if (setjmp(png_jmpbuf(png_handler.png_ptr))) {
+    PNG_CLEANUP
+    return 0;
+  }
 
-    png_set_IHDR(
-      png_ptr, info_ptr,
-      width, height,
-      bit_depth, color_type,
-      interlace_type, compression_type, filter_type
-    );
+  png_set_IHDR(
+    png_handler.png_ptr, png_handler.info_ptr,
+    width, height,
+    bit_depth, color_type,
+    interlace_type, compression_type, filter_type
+  );
 
-    png_write_info(png_ptr, info_ptr);
+  // reset error handler to put png_deleter into scope.
+  if (setjmp(png_jmpbuf(png_handler.png_ptr))) {
+    PNG_CLEANUP
+    return 0;
+  }
 
-    // Allocate row buffer
-    size_t rowbytes = png_get_rowbytes(png_ptr, info_ptr);
-    row_ptr = png_malloc(png_ptr, rowbytes);
-    if (!row_ptr) break;
+  png_write_info(png_handler.png_ptr, png_handler.info_ptr);
 
-    // Generate and write image data
-    size_t input_index = 7;
-    for (uint32_t y = 0; y < height; ++y) {
-      // Fill row with fuzzed data or repeating pattern
-      for (size_t x = 0; x < rowbytes; ++x) {
-        png_bytep row = static_cast<png_bytep>(row_ptr);
-        row[x] = data[input_index++ % size];  // wrap around data
-      }
-      png_write_row(png_ptr, static_cast<png_bytep>(row_ptr));
+  // Allocate row buffer
+  png_handler.row_ptr = png_malloc(
+      png_handler.png_ptr, png_get_rowbytes(png_handler.png_ptr,
+                                           png_handler.info_ptr));
+  if (!png_handler.row_ptr) {
+    PNG_CLEANUP
+    return 0;
+  }
+
+  // Generate and write image data
+  size_t input_index = 7;
+  for (uint32_t y = 0; y < height; ++y) {
+    // Fill row with fuzzed data or repeating pattern
+    png_bytep row = static_cast<png_bytep>(png_handler.row_ptr);
+    for (size_t x = 0; x < png_get_rowbytes(png_handler.png_ptr, png_handler.info_ptr); ++x) {
+      row[x] = data[input_index++ % size];  // wrap around data
     }
+    png_write_row(png_handler.png_ptr, row);
+  }
 
-    png_write_end(png_ptr, nullptr);
+  png_write_end(png_handler.png_ptr, nullptr);
 
-  } while (0);  // Single-iteration do-while for clean error handling
-
-  // Cleanup
-  if (row_ptr && png_ptr)
-    png_free(png_ptr, row_ptr);
-  if (info_ptr && png_ptr)
-    png_destroy_write_struct(&png_ptr, &info_ptr);
-  else if (png_ptr)
-    png_destroy_write_struct(&png_ptr, nullptr);
-
+  PNG_CLEANUP
   return 0;
 }
