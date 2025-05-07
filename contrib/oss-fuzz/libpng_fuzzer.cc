4a06516a5d9d4411b1088808a774234a7e22dd5c
Ahyoung Seo <113351371+ahzero7d1@users.noreply.github.com>
2025-05-08 01:41:34 +0200

    Update libpng_fuzzer.cc
    disable symbol renaming

--- contrib/oss-fuzz/libpng_fuzzer.cc
+++ contrib/oss-fuzz/libpng_fuzzer.cc
@@ -21,6 +21,13 @@
 #define PNG_INTERNAL
 #include "png.h"
 
+#undef png_create_write_struct
+#undef png_set_write_fn
+#undef png_write_info
+#undef png_write_row
+#undef png_write_end
+#undef png_destroy_write_struct
+
 #define PNG_CLEANUP_READ                                                \
   if(png_handler.png_ptr) {                                             \
     if (png_handler.row_ptr)                                            \
