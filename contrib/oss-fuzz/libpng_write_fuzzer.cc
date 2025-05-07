2bc7a480ae525138e4db565bda67553e92551c61
Rodrigue de Guerre <99824035+Rodrigue2g@users.noreply.github.com>
2025-05-07 23:42:31 +0200

    Update libpng_write_fuzzer.cc

--- contrib/oss-fuzz/libpng_write_fuzzer.cc
+++ contrib/oss-fuzz/libpng_write_fuzzer.cc
@@ -35,21 +35,47 @@ struct WriteBuffer {
   std::vector<uint8_t> data;
 };
 
+struct BufState {
+  const uint8_t* data;
+  size_t bytes_left;
+};
+
+// struct PngObjectHandler {
+//   png_infop info_ptr = nullptr;
+//   png_structp png_ptr = nullptr;
+//   png_infop end_info_ptr = nullptr;
+//   png_voidp row_ptr = nullptr;
+//   WriteBuffer* write_buf = nullptr;
+
+//   ~PngObjectHandler() {
+//     if (row_ptr)
+//       png_free(png_ptr, row_ptr);
+//     if (info_ptr)
+//       png_destroy_write_struct(&png_ptr, &info_ptr);
+//     else
+//       png_destroy_write_struct(&png_ptr, nullptr);
+//     delete write_buf;
+//   }
+// };
+
 struct PngObjectHandler {
   png_infop info_ptr = nullptr;
   png_structp png_ptr = nullptr;
   png_infop end_info_ptr = nullptr;
   png_voidp row_ptr = nullptr;
+  // BufState* buf_state = nullptr;
   WriteBuffer* write_buf = nullptr;
 
   ~PngObjectHandler() {
     if (row_ptr)
       png_free(png_ptr, row_ptr);
-    if (info_ptr)
-      png_destroy_write_struct(&png_ptr, &info_ptr);
+    if (end_info_ptr)
+      png_destroy_read_struct(&png_ptr, &info_ptr, &end_info_ptr);
+    else if (info_ptr)
+      png_destroy_read_struct(&png_ptr, &info_ptr, nullptr);
     else
-      png_destroy_write_struct(&png_ptr, nullptr);
-    delete write_buf;
+      png_destroy_read_struct(&png_ptr, nullptr, nullptr);
+    delete buf_state;
   }
 };
 
