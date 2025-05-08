c2b946b3dbd9703bbb7238a1ade7d0f3acf2b164
Rodrigue de Guerre <99824035+Rodrigue2g@users.noreply.github.com>
2025-05-08 16:36:04 +0200

    Update build.sh

--- contrib/oss-fuzz/build.sh
+++ contrib/oss-fuzz/build.sh
@@ -30,6 +30,13 @@
 # > scripts/pnglibconf.dfa.temp
 # mv scripts/pnglibconf.dfa.temp scripts/pnglibconf.dfa
 
+# Disable logging via library build configuration control.
+cat scripts/pnglibconf.dfa | \
+  sed -e "s/option STDIO/option STDIO disabled/" \
+      -e "s/option WARNING /option WARNING disabled/" \
+> scripts/pnglibconf.dfa.temp
+mv scripts/pnglibconf.dfa.temp scripts/pnglibconf.dfa
+
 # build the libpng library.
 autoreconf -f -i
 ./configure --with-libpng-prefix=OSS_FUZZ_
