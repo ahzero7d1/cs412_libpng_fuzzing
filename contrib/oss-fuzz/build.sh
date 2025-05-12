d5423f2fa10f8ed66e1028b6e9c0ad447437148c
Ahyoung Seo <113351371+ahzero7d1@users.noreply.github.com>
2025-05-12 12:09:51 +0200

    Update build.sh

--- contrib/oss-fuzz/build.sh
+++ contrib/oss-fuzz/build.sh
@@ -35,16 +35,10 @@ autoreconf -f -i
 make -j$(nproc) clean
 make -j$(nproc) libpng16.la
 
-# build libpng_read_fuzzer.
+# build libpng_fuzzer.
 $CXX $CXXFLAGS -std=c++11 -I. \
-     $SRC/libpng/contrib/oss-fuzz/libpng_read_fuzzer.cc \
-     -o $OUT/libpng_read_fuzzer \
-     -lFuzzingEngine .libs/libpng16.a -lz
-
-# build libpng_write_fuzzer.
-$CXX $CXXFLAGS -std=c++11 -I. \
-     $SRC/libpng/contrib/oss-fuzz/libpng_write_fuzzer.cc \
-     -o $OUT/libpng_write_fuzzer \
+     $SRC/libpng/contrib/oss-fuzz/libpng_fuzzer.cc \
+     -o $OUT/libpng_fuzzer \
      -lFuzzingEngine .libs/libpng16.a -lz
 
 # add seed corpus.
